01da8d49b73f8c6ded31676de3334b6c83704e2c
Akshita Dixit <56997545+akshitadixit@users.noreply.github.com>
2020-11-18 21:41:53 +0530

    Edit image file paths

--- m8b.py
+++ m8b.py
@@ -3,9 +3,9 @@ import random
 c1 = []
 c2 = []
 
-positive = ['images/glow ('+str(x)+').png' for x in range(10)]
-neutral = ['images/glow ('+str(x)+').png' for x in range(10,15)]
-negative = ['images/glow ('+str(x)+').png' for x in range(15,20)]
+positive = ['images1/glow('+str(x)+').png' for x in range(10)]
+neutral = ['images1/glow('+str(x)+').png' for x in range(10,15)]
+negative = ['images1/glow('+str(x)+').png' for x in range(15,20)]
 
 def ANSWER():
 	c1 = random.choice([positive, negative, neutral])
